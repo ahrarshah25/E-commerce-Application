9cdfb63ecef12bf4bd61dfcf1502fa60b417ee92
Ahrar Shah <ahrar.09321@gmail.com>
2025-11-06 21:19:15 -0800

    Checkout Incomplete

--- js/checkout.js
+++ js/checkout.js
@@ -1,5 +1,22 @@
 console.log("Checkout Page!");
 
 
-document.getElementById("userEmail").value = localStorage.getItem("userEmail");
-document.getElementById("userName").textContent = localStorage.getItem("userName");
+var userEmail = document.getElementById("userEmail").value = localStorage.getItem("userEmail");
+var userName = document.getElementById("userName").value = localStorage.getItem("userName");
+
+function placeOrder(event){
+    event.preventDefault();
+    var userAddress = document.getElementById("userAddress").value.trim();
+    var userContact = document.getElementById("userContact").value.trim();
+    var userOrderProduct = localStorage.getItem("userOrderProduct");
+
+    
+    if(userAddress === "" || userContact === ""){
+        alert("Please Fill All The Feilds To Complete Your Order");
+    }else if(userContact < 11 || userContact < 12){
+        alert("Please Enter Corrrect Number!");
+    }else{
+       alert("Hello");
+    }
+    
+}

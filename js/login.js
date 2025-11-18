7d1b19b10b95dbb55c364c3c9ebb9c79b853ec8b
Ahrar Shah <ahrar.09321@gmail.com>
2025-11-18 21:47:55 +0500

    Supabase Added In Signup + Login & EventListener Used

--- js/login.js
+++ js/login.js
@@ -1,25 +1,56 @@
 console.log("Login Page!");
 
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const supabaseUrl = 'https://uhvbtcngusubtvtqgyzc.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVodmJ0Y25ndXN1YnR2dHFneXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NzEwMjYsImV4cCI6MjA3OTA0NzAyNn0.jc_mzh6JqtaIlKnQwehZktJh8LtPiV398G0riVMzP9Y'
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 var userFound = localStorage.getItem("userFound")
-if(userFound === "true"){
+if (userFound === "true") {
     window.location.href = "index.html"
 }
 
 
-function userLogin(event){
+function userLogin(event) {
     event.preventDefault();
     var userEmail = document.getElementById("email").value.trim();
     var userPassword = document.getElementById("password").value.trim();
     var regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    if(userEmail === "" || userPassword === ""){
+    if (userEmail === "" || userPassword === "") {
         alert("Please Enter Email + Password For Login!");
-    }else if(!regex.test(userEmail)){
+    } else if (!regex.test(userEmail)) {
         alert("Please Enter Email With Correct Syntax\nFor Example: name@domain.com")
-    }else if(userEmail !== localStorage.getItem("userEmail") || userPassword !== localStorage.getItem("userPassword")){
-        alert("Please Enter Correct Email & Password!\nOR\nIf You Are Not Signup So Please Signup First!");
-    }else{
-        localStorage.setItem("userFound" , "true");
+    } else {
+
+        const { data: loginData, error: loginError } = supabase.auth.signInWithPassword({
+            email: userEmail,
+            password: userPassword
+        });
+
+        if(loginError){
+            alert("Error During Login: " + loginError.message);
+            return;
+        }
+
+        // const username = loginData.user.user_metadata.username;
+        alert("Login Successfully!");
+        localStorage.setItem("userFound", "true");
         window.location.href = "index.html"
     }
 }
+
+let tooglePassword = document.getElementById("toggle-password");
+tooglePassword.addEventListener("click", function(){
+    let passwordField = document.getElementById("password");
+    if(passwordField.type === "password"){
+        passwordField.type = "text";
+        tooglePassword.textContent = "Hide";
+    }else{
+        passwordField.type = "password";
+        tooglePassword.textContent = "Show";
+    }
+});
+
+let loginBtn = document.getElementById("userLogin");
+loginBtn.addEventListener("click", userLogin);
--- js/signup.js
+++ js/signup.js
@@ -1,12 +1,18 @@
 console.log("Signup Page!");
 
-var userFound = localStorage.getItem("userFound")
+
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const supabaseUrl = 'https://uhvbtcngusubtvtqgyzc.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVodmJ0Y25ndXN1YnR2dHFneXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NzEwMjYsImV4cCI6MjA3OTA0NzAyNn0.jc_mzh6JqtaIlKnQwehZktJh8LtPiV398G0riVMzP9Y'
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+var userFound = localStorage.getItem("userFound");
 if(userFound === "true"){
     window.location.href = "index.html"
 }
 
 
-function userSignup(event){
+async function userSignup(event){
     event.preventDefault();
     
     var userName = document.getElementById("fullname").value.trim();
@@ -25,11 +31,29 @@ function userSignup(event){
     }else if(userConfirmPassword !== userPassword){
         alert("Please Enter Correct Password In Confirm Password Feild!")
     }else{
+        const {data: loginData, error: loginError} = await supabase.auth.signUp({
+            email: userEmail,
+            password: userPassword,
+            options: {
+              data: {
+                 username: userName
+                }
+            }
+        });
+
+        if(loginError){
+            alert("Error During Signup: " + loginError.message);
+            return;
+        }else{
         localStorage.setItem("userName" , userName);
         localStorage.setItem("userEmail" , userEmail);
         localStorage.setItem("userPassword" , userPassword);
         localStorage.setItem("userConfirmPassword" , userConfirmPassword);
         alert("Signup Successfully!");
-        window.location.href= "login.html"
+        window.location.href= "login.html";
+        }
     }
 }
+
+let signupBtn = document.getElementById("userSignup");
+signupBtn.addEventListener("click", userSignup);
